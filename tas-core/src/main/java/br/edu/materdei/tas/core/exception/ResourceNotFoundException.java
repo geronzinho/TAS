24b09ace6adfac2202cfb8e48f6610f40a5c92e1
Alexandre <Alexandre@INFRA-GI-000533>
2020-09-02 16:14:55 -0300

    aula 26-08-2020

---
+++ tas-core/src/main/java/br/edu/materdei/tas/core/exception/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package br.edu.materdei.tas.core.exception;
+
+
+
+public class ResourceNotFoundException extends Exception {
+
+    public ResourceNotFoundException(Integer resourceId) {
+        super(resourceId != null ? resourceId.toString() : null);
+    }
+    
+}
